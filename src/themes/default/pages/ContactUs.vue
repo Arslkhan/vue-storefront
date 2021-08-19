e31dc24aa953890403b27a19386af078bd2712de
arslkhan <arslkhan5@gmail.com>
2021-08-20 00:36:57 +0500

    form work

--- src/themes/default/pages/ContactUs.vue
+++ src/themes/default/pages/ContactUs.vue
@@ -47,8 +47,160 @@
 </template>
 
 <script>
-export default {
+import { registerModule } from '@vue-storefront/core/lib/modules'
+import { MailerModule } from '@vue-storefront/core/modules/mailer'
+import { EmailForm } from '@vue-storefront/core/modules/mailer/components/EmailForm'
+import i18n from '@vue-storefront/i18n'
+import config from 'config';
 
+export default {
+  data () {
+    return {
+      PageName: 'Contact Us',
+      form: null,
+      submitBtn: null,
+      successBtnBox: null
+    };
+  },
+  beforeCreate () {
+    registerModule(MailerModule)
+  },
+  computed: {
+    sourceAddress () {
+      return config.mailer.sourceAddress
+    },
+    cmsPageContent () {
+      return this.$store.state.cmsPage.current;
+    },
+    contactFormEmailAddress () {
+      return config.mailer.targetAddress
+    },
+    contactFormSubject () {
+      return 'General Inquiries'
+    }
+  },
+  methods: {
+    successMessage () {
+      return '<div data-v-53963ae7="" class="col-xs-12 confirmation"><h3 data-v-53963ae7="" class="py15 px10 my40 weight-600 flex cl-white lightGreybar brdr-rad"><i data-v-53963ae7="" class="material-icons">check_circle_outline</i> Your message has successfully been sent.</h3></div>'
+    },
+    sendEmail (letter, success, failure) {
+      this.$store.dispatch('mailer/sendEmail', letter)
+        .then(res => {
+          if (res.ok) {
+            if (success) success(i18n.t('Email has successfully been sent'))
+          } else {
+            return res.json()
+          }
+        })
+        .then(errorResponse => {
+          if (errorResponse) {
+            const errorMessage = errorResponse.result
+            if (failure) failure(i18n.t(errorMessage))
+          }
+        })
+        .catch(() => {
+          if (failure) failure(i18n.t('Could not send an email. Please try again later.'))
+        })
+    },
+    formBodyText ({ firstName, lastName, email, phone, subject }) {
+      let html = '';
+
+      html += 'Name: ' + firstName + ' ' + lastName + '\r\n\r\n'
+      html += 'Email Address: ' + email + '\r\n\r\n'
+      if (phone) {
+        html += 'Phone Number: ' + phone + '\r\n\r\n'
+      }
+      if (this.subject) {
+        html += 'Subject: ' + subject + '\r\n\r\n'
+      }
+      html += 'Message: ' + subject + '\r\n\r\n'
+
+      return html
+    },
+
+    onSuccess (message) {
+      this.messageSent = true
+      this.form.remove();
+      this.submitBtn.remove();
+      this.successBtnBox.innerHTML = this.successMessage();
+
+      if (config.mailer.sendConfirmation) {
+        let confirmationHtml = '';
+        confirmationHtml += 'Dear customer,\r\n\r\n'
+        confirmationHtml += 'We have received your request.\r\n\r\n'
+        confirmationHtml += 'Thank you!'
+
+        this.sendEmail(
+          {
+            sourceAddress: this.sourceAddress,
+            targetAddress: this.contactFormEmailAddress,
+            subject: this.$t('Confirmation of receival'),
+            emailText: confirmationHtml,
+            confirmation: true
+          })
+      }
+    },
+    onFailure (message) {
+      this.$store.dispatch('notification/spawnNotification', {
+        type: 'error',
+        message,
+        action1: { label: this.$t('OK') }
+      })
+    }
+  },
+  updated () {
+    let contactForm = document.querySelector('div#contact-us-form');
+    console.log('contactFormWouldBe', contactForm);
+  },
+  mounted () {
+    if (this.cmsPageContent.title === this.PageName) {
+      this.form = document.querySelector('div#contact-us-form');
+      this.submitBtn = document.querySelector('button.button-contact');
+      this.successBtnBox = document.querySelector('div.buttonmessage');
+      console.log('submitBtn', this.successBtnBox);
+      // form.addEventListener('submit', (e) => {
+      //   e.preventDefault();
+      //   const formData = new FormData(e.target);
+      //   console.log('FormDataWouldBe', formData);
+      //   // Now you can use formData.get('foo'), for example.
+      //   // Don't forget e.preventDefault() if you want to stop normal form .submission
+      // });
+      document.querySelector('button.button-contact').onclick = () => {
+        // let firstName = document.querySelector("input[name='fname']");
+        let firstName = document.getElementById('fname').value;
+        let lastName = document.getElementById('lname').value;
+        let email = document.getElementById('email').value;
+        let phone = document.getElementById('phone').value;
+        let message = document.getElementById('message').value;
+        console.log('firstName', firstName, lastName, email, phone, message);
+        let formBodyText = this.formBodyText({ firstName, lastName, email, phone, subject: this.contactFormSubject })
+        let data = {
+          sourceAddress: this.sourceAddress,
+          replyAddress: email,
+          targetAddress: this.contactFormEmailAddress,
+          emailText: formBodyText,
+          confirmation: true,
+          subject: this.contactFormSubject
+        };
+        if (firstName && lastName && email && phone && message) {
+          this.sendEmail(
+            data,
+            this.onSuccess,
+            this.onFailure
+          )
+        }
+
+
+        console.log('firstName', data);
+        // form.submit();
+      }
+      // let contactForm = document.getElementById('contact-us-form');
+      // let formData = new FormData(contactForm)
+      // console.log('formSHouldBe', contactForm, formData);
+      console.log('its a contact Us page.');
+    }
+  },
+  mixins: [CmsPage, EmailForm]
 }
 </script>
 
