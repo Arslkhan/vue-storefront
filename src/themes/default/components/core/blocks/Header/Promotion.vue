ac8300bb5efe84bc24032ab428e1b56e7f6d12c3
arslkhan <arslkhan5@gmail.com>
2021-12-05 20:25:53 +0500

    sale heading

--- src/themes/default/components/core/blocks/Header/Promotion.vue
+++ src/themes/default/components/core/blocks/Header/Promotion.vue
@@ -1,12 +1,12 @@
 <template>
    <div>
     <div class="promotion-bar">
-    <div class="sale-heading">
-        <router-link :to="localizedRoute('/our-range.html')" class="sale-class">
-            <span class="first">SALE</span> <br>
-            <span class="second"> UP TO 50% OFF</span> 
-        </router-link>
-    </div>
+        <div class="sale-heading">
+            <router-link :to="localizedRoute('/our-range.html')" class="sale-class">
+                <span class="first">SALE</span> <br>
+                <span class="second"> UP TO 50% OFF</span> 
+            </router-link> 
+        </div>
     <img src="assets/promotionbar.png" alt="sale-promotion">
   </div>    
 </div>
@@ -21,5 +21,8 @@ export default {
 <style lang="css">
 .sale-class {
     color: #ffffff;
+}
+.sale-heading {
+    position: absolute;
 }
 </style>
