9956e67d794e1057a7f9bb6d92807712d33fb300
arslanki5 <arsalan.khan@ki5.co.uk>
2021-12-13 19:21:37 +0500

    promotion bar change

--- src/themes/default/components/core/blocks/Header/Promotion.vue
+++ src/themes/default/components/core/blocks/Header/Promotion.vue
@@ -1,15 +1,15 @@
 <template>
-   <div>
+  <div>
     <div class="promotion-bar">
-        <div class="sale-heading">
-            <router-link :to="localizedRoute('/our-range.html')" class="sale-class">
-                <span class="first">SALE</span> <br>
-                <span class="second"> UP TO 50% OFF</span> 
-            </router-link> 
-        </div>
-    <img src="assets/promotionbar.png" alt="sale-promotion">
-  </div>    
-</div>
+      <div class="sale-heading">
+        <router-link :to="localizedRoute('/our-range.html')" class="sale-class">
+          <span class="first">SALE</span> <br>
+          <span class="second"> UP TO 50% OFF</span>
+        </router-link>
+      </div>
+      <img src="assets/sale_banner_background.png" alt="sale-promotion">
+    </div>
+  </div>
 </template>
 
 <script>
